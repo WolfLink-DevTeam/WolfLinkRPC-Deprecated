266bf4a910d9d898c0e50e2216eebf8a75fe62a6
米可绫夏 <77883323+MikkoAyaka@users.noreply.github.com>
2023-01-27 17:26:40 +0800

    删除跟Bukkit无关的

--- Windows-WolfLinkRPC/src/main/java/entity/MCServerDataClass.kt
+++
@@ -1,8 +0,0 @@
-package org.wolflink.windows.wolflinkrpc.entity
-
-import org.wolflink.common.wolflinkrpc.api.interfaces.JsonSerializable
-
-data class MCServerDataClass(val name : String,val windowTitle : String,val folderPath : String,val batName :String)
-{
-    companion object : JsonSerializable<MCServerDataClass>
-}
