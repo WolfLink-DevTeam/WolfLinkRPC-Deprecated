d23810f24a2d638299c1e44d9b4c85bcff24c2be
MikkoAyaka <3401286177@qq.com>
2023-02-05 10:47:56 +0800

    修复了服务器内验证失败的bug

--- Bukkit-WolfLinkRPC/src/main/java/org/wolflink/paper/wolflinkrpc/listener/OnPlayerJoin.java
+++ Bukkit-WolfLinkRPC/src/main/java/org/wolflink/paper/wolflinkrpc/listener/OnPlayerJoin.java
@@ -22,33 +22,36 @@ public class OnPlayerJoin implements Listener {
     @EventHandler
     public void onLogin(PlayerJoinEvent e)
     {
-        String routingKey = "mirai_WolfBot+";
-        String message = "验证绑定";
-        Player p = e.getPlayer();
-        RPCDataPack datapack = new RPCDataPack.Builder()
-                .setType(DataPackType.COMMAND_EXECUTE)
-                .setDatapackBody(new SimpleCommandExecuteBody(message))
-                .setSender(new RPCUser(App.RPC_CONFIGURATION.getQueueName(),
-                        ClientType.BUKKIT,
-                        p.getName(),
-                        p.getName(),
-                        new RoutingData().addRoutingKey(App.RPC_CONFIGURATION.getQueueName())))
-                .addReceiver(new ClientReceiver(routingKey))
-                .build();
-        MQService.INSTANCE.sendDataPack(datapack, true, new CallbackFunction() {
-            @Override
-            public void success(@NotNull RPCDataPack rpcDataPack)
-            {
-                if(!rpcDataPack.getJsonObject().get("result").getAsBoolean())
+        if(MQService.INSTANCE.getConnection().isOpen())
+        {
+            String routingKey = "mirai_WolfBot+";
+            String message = "验证绑定";
+            Player p = e.getPlayer();
+            RPCDataPack datapack = new RPCDataPack.Builder()
+                    .setType(DataPackType.COMMAND_EXECUTE)
+                    .setDatapackBody(new SimpleCommandExecuteBody(message))
+                    .setSender(new RPCUser(App.RPC_CONFIGURATION.getQueueName(),
+                            ClientType.BUKKIT,
+                            p.getName(),
+                            p.getName(),
+                            new RoutingData().addRoutingKey(App.RPC_CONFIGURATION.getQueueName())))
+                    .addReceiver(new ClientReceiver(routingKey))
+                    .build();
+            MQService.INSTANCE.sendDataPack(datapack, true, new CallbackFunction() {
+                @Override
+                public void success(@NotNull RPCDataPack rpcDataPack)
                 {
-                    Bukkit.getScheduler().runTask(App.INSTANCE,()->p.kickPlayer("\u00a7cQQ绑定验证失败！\u00a7f请前往群内绑定你的游戏账户与QQ号。"));
+                    if(!rpcDataPack.getJsonObject().get("result").getAsBoolean())
+                    {
+                        Bukkit.getScheduler().runTask(App.INSTANCE,()->p.kickPlayer("\u00a7cQQ绑定验证失败！\u00a7f请前往群内绑定你的游戏账户与QQ号。"));
+                    }
                 }
-            }
-            @Override
-            public void failed(@NotNull RPCDataPack rpcDataPack) {
-                Bukkit.getScheduler().runTask(App.INSTANCE,()->
-                        p.kickPlayer("\u00a7cQQ绑定验证失败！\u00a77可能是网络连接不畅，请重试。多次尝试无果请联系 \u00a7fQQ 3401286177"));
-            }
-        },10);
+                @Override
+                public void failed(@NotNull RPCDataPack rpcDataPack) {
+                    Bukkit.getScheduler().runTask(App.INSTANCE,()->
+                            p.kickPlayer("\u00a7cQQ绑定验证失败！\u00a77可能是网络连接不畅，请重试。多次尝试无果请联系 \u00a7fQQ 3401286177"));
+                }
+            },10);
+        }
     }
 }
