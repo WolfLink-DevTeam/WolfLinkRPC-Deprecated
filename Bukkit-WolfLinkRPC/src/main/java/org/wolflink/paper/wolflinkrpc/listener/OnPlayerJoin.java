679cf01e6afed9213796cb15a9904a25c3f268c3
MikkoAyaka <3401286177@qq.com>
2023-03-07 14:51:12 +0800

    玩家加入服务器时验证逻辑补全

--- Bukkit-WolfLinkRPC/src/main/java/org/wolflink/paper/wolflinkrpc/listener/OnPlayerJoin.java
+++ Bukkit-WolfLinkRPC/src/main/java/org/wolflink/paper/wolflinkrpc/listener/OnPlayerJoin.java
@@ -33,11 +33,15 @@ public class OnPlayerJoin implements Listener {
                 .build();
         MQService.INSTANCE.sendDataPack(datapack, true, new CallbackFunction() {
             @Override
-            public void success(@NotNull RPCDataPack rpcDataPack) {}
-
+            public void success(@NotNull RPCDataPack rpcDataPack) {
+                if(!rpcDataPack.getJsonObject().get("result").getAsBoolean())
+                {
+                    p.kickPlayer("\u00a7cQQ绑定验证失败！\u00a77但网络连接通畅。请联系 \u00a7fQQ 3401286177");
+                }
+            }
             @Override
             public void failed(@NotNull RPCDataPack rpcDataPack) {
-                p.kickPlayer("QQ绑定验证失败！请前往群内绑定你的游戏账户与QQ号。");
+                p.kickPlayer("\u00a7cQQ绑定验证失败！\u00a77可能是网络连接不畅，请重试。多次尝试无果请联系 \u00a7fQQ 3401286177");
             }
         },10);
     }
